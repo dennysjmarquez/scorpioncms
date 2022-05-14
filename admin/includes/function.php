f6d0303280f1ed46c9ab9460d21c93c074ced927
Dennys J Marquez <dennysjmarquez@gmail.com>
2022-05-13 20:49:19 -0400

    "php": "7.0.0"

--- admin/includes/function.php
+++ admin/includes/function.php
@@ -1,191 +1,221 @@
 <?php
 
-function load($file, $views = false){
-	
-	if($views == false){
-		
-		global $Globals;
-		extract($Globals->GetAll());
-		
-		if(is_file($file)){
-			
-			require($file);
-		}
-	
-	}elseif($views == true){
-		
-		global $QueryAdmin, 
-		$CorelAdmin, $Globals, 
-		$lang, $themes, 
-		$modulo, $controller, 
-		$action, $template;
-	
-		extract($Globals->GetAll());
-		
-		if(is_file($file)){
-			
-			$themes->GetThemes();
-			require($file);
+function load($file, $views = false)
+{
 
-		}			
-		
-	}
-	
-} function ajaxMsg($msg = '', $callback = '', $status = 302){
-	
-	$data = [
-		
-		'ERROR_MESSAGE' => lang_s($msg, true),
-		'CALLBACK' => $callback
-		
-			];
-		
-	header("Content-Type: application/json; charset=utf-8", true);
-	header("HTTP/1.1 ".$status." ".lang_s($msg, true)); 
-	echo json_encode($data);
-	exit;
-	
-}function redirect($url, $ajax = false, $msg = '', $callback = '', $status = 302){
-	
-	if($ajax === true){
-		
-		$data = [
-		
-			'ERROR_MESSAGE' => lang_s($msg, true),
-			'CALLBACK' => $callback
-		
-				];
-		
-		header("Content-Type: application/json; charset=utf-8", true);
-		header("HTTP/1.1 ".$status." ".lang_s($msg, true)); 
-		echo json_encode($data);
-		exit;
-		
-	
-	}elseif($ajax === false){
-	
-		header('Location: ' . $url, true, $status);
-		exit;
-	
-	}
-	
-} function tag_check($orig, $new) {
+    if ($views == false) {
 
-	$add = null;
-	$del = null;
-  
-	foreach ($orig as $key => $value) {
-		if (!in_array($value, $new)) {
-			$del[] = $value;
-		}
-	}
-	foreach ($new as $key => $value) {
-		if (!in_array($value, $orig)) {
-			$add[] = $value;
-		}
-	}
-  
-	$return = ['deleted'=> $del, 'add' => $add];
-  
-  return $return;
-} function msg($msg){
-	
-	switch ($msg['type']) {
-            
-		case 'danger':
-		
-			return '<div class="col-md-6" style="position: absolute; margin-left: 25%; margin-right: 25%; top: 25%;">
-					<div class="alert alert-danger alert-dismissable">
-						<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-						<h4><i class="icon fa fa-ban"></i>'.$msg['title'].'</h4>
-						'.$msg['msg'].'
-					</div>
-				</div>';
-		break;
-	}	
-	
-	$Globals->add("msg", null);
+        global $Globals;
+        extract($Globals->GetAll());
 
-} function slug_str_split($string) {
-   
-   $slen = strlen($string);
-   
-   for($i=0; $i<$slen; $i++) {
-      
-	  $sArray[$i] = $string{$i};
-   
-   }
-   
-   return $sArray;
+        if (is_file($file)) {
 
-} function slug_noDiacritics($string) {
-   
-	$cyrylicFrom = array('?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?');
-    $cyrylicTo   = array('A', 'B', 'W', 'G', 'D', 'Ie', 'Io', 'Z', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'Ch', 'C', 'Tch', 'Sh', 'Shtch', '', 'Y', '', 'E', 'Iu', 'Ia', 'a', 'b', 'w', 'g', 'd', 'ie', 'io', 'z', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'ch', 'c', 'tch', 'sh', 'shtch', '', 'y', '', 'e', 'iu', 'ia'); 
-
-    $from = array("Á", "À", "Â", "Ä", "A", "A", "Ã", "Å", "A", "Æ", "C", "C", "C", "C", "Ç", "D", "Ð", "Ð", "É", "È", "E", "Ê", "Ë", "E", "E", "E", "?", "G", "G", "G", "G", "á", "à", "â", "ä", "a", "a", "ã", "å", "a", "æ", "c", "c", "c", "c", "ç", "d", "d", "ð", "é", "è", "e", "ê", "ë", "e", "e", "e", "?", "g", "g", "g", "g", "H", "H", "I", "Í", "Ì", "I", "Î", "Ï", "I", "I", "?", "J", "K", "L", "L", "N", "N", "Ñ", "N", "Ó", "Ò", "Ô", "Ö", "Õ", "O", "Ø", "O", "Œ", "h", "h", "i", "í", "ì", "i", "î", "ï", "i", "i", "?", "j", "k", "l", "l", "n", "n", "ñ", "n", "ó", "ò", "ô", "ö", "õ", "o", "ø", "o", "œ", "R", "R", "S", "S", "Š", "S", "T", "T", "Þ", "Ú", "Ù", "Û", "Ü", "U", "U", "U", "U", "U", "U", "W", "Ý", "Y", "Ÿ", "Z", "Z", "Ž", "r", "r", "s", "s", "š", "s", "ß", "t", "t", "þ", "ú", "ù", "û", "ü", "u", "u", "u", "u", "u", "u", "w", "ý", "y", "ÿ", "z", "z", "ž");
-    $to   = array("A", "A", "A", "A", "A", "A", "A", "A", "A", "AE", "C", "C", "C", "C", "C", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "G", "G", "G", "G", "G", "a", "a", "a", "a", "a", "a", "a", "a", "a", "ae", "c", "c", "c", "c", "c", "d", "d", "d", "e", "e", "e", "e", "e", "e", "e", "e", "g", "g", "g", "g", "g", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "IJ", "J", "K", "L", "L", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "CE", "h", "h", "i", "i", "i", "i", "i", "i", "i", "i", "ij", "j", "k", "l", "l", "n", "n", "n", "n", "o", "o", "o", "o", "o", "o", "o", "o", "o", "R", "R", "S", "S", "S", "S", "T", "T", "T", "U", "U", "U", "U", "U", "U", "U", "U", "U", "U", "W", "Y", "Y", "Y", "Z", "Z", "Z", "r", "r", "s", "s", "s", "s", "B", "t", "t", "b", "u", "u", "u", "u", "u", "u", "u", "u", "u", "u", "w", "y", "y", "y", "z", "z", "z");
-
-    $from = array_merge($from, $cyrylicFrom);
-    $to   = array_merge($to, $cyrylicTo);
-
-    $newstring=str_replace($from, $to, $string);
-    return $newstring;
-
-} function makeSlugs($string, $maxlen=200) {
-    
-	$newStringTab=array();
-    $tab = null ;
-    $stringTab=str_split(strtolower(slug_noDiacritics($string)));
-    $authorizedArray=array_merge(range('a', 'z'), array('0','1','2','3','4','5','6','7','8','9','-'));
-
-    foreach($stringTab as $letter) {
-        
-		if(in_array($letter, $authorizedArray)) {
-        
-			$tab=$letter;
-            $newStringTab[] = $tab ;
-        
-		} else if($tab != '-') {
-            
-			$tab='-';
-            
-			$newStringTab[] = $tab ;
+            require($file);
         }
 
-	}
-		
-	if(count($newStringTab)) {
-    
-		$newString=implode($newStringTab);
-    
-		if(!is_null($maxlen)){
-			$newString=substr($newString, 0, $maxlen);
-		}
-   
-   } else {
-   
-	$newString='';   
-   
-   }
-   
-   return trim( $newString, '-' );
+    } elseif ($views == true) {
 
-} function checkSlug($sSlug) {
-   
-   return preg_match("/^[a-zA-Z0-9]+[a-zA-Z0-9\_\-]*$/", $sSlug);
+        global $QueryAdmin,
+               $CorelAdmin, $Globals,
+               $lang, $themes,
+               $modulo, $controller,
+               $action, $template;
 
-} function minify_html($input) {
-    if(trim($input) === "") return $input;
-	
-	
+        extract($Globals->GetAll());
+
+        if (is_file($file)) {
+
+            $themes->GetThemes();
+            require($file);
+
+        }
+
+    }
+
+}
+
+function ajaxMsg($msg = '', $callback = '', $status = 302)
+{
+
+    $data = [
+
+        'ERROR_MESSAGE' => lang_s($msg, true),
+        'CALLBACK' => $callback
+
+    ];
+
+    header("Content-Type: application/json; charset=utf-8", true);
+    header("HTTP/1.1 " . $status . " " . lang_s($msg, true));
+    echo json_encode($data);
+    exit;
+
+}
+
+function redirect($url, $ajax = false, $msg = '', $callback = '', $status = 302)
+{
+
+    if ($ajax === true) {
+
+        $data = [
+
+            'ERROR_MESSAGE' => lang_s($msg, true),
+            'CALLBACK' => $callback
+
+        ];
+
+        header("Content-Type: application/json; charset=utf-8", true);
+        header("HTTP/1.1 " . $status . " " . lang_s($msg, true));
+        echo json_encode($data);
+        exit;
+
+
+    } elseif ($ajax === false) {
+
+        header('Location: ' . $url, true, $status);
+        exit;
+
+    }
+
+}
+
+function tag_check($orig, $new)
+{
+
+    $add = null;
+    $del = null;
+
+    foreach ($orig as $key => $value) {
+        if (!in_array($value, $new)) {
+            $del[] = $value;
+        }
+    }
+    foreach ($new as $key => $value) {
+        if (!in_array($value, $orig)) {
+            $add[] = $value;
+        }
+    }
+
+    $return = ['deleted' => $del, 'add' => $add];
+
+    return $return;
+}
+
+function msg($msg)
+{
+
+    var_dump($msg);
+
+    switch ($msg['type']) {
+
+        case 'danger':
+
+            return '<div class="col-md-6" style="position: absolute; margin-left: 25%; margin-right: 25%; top: 25%;">
+					<div class="alert alert-danger alert-dismissable">
+						<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+						<h4><i class="icon fa fa-ban"></i>' . $msg['title'] . '</h4>
+						' . $msg['msg'] . '
+					</div>
+				</div>';
+            break;
+    }
+
+    $Globals->add("msg", null);
+
+}
+
+function slug_str_split($string)
+{
+
+    $slen = strlen($string);
+
+    for ($i = 0; $i < $slen; $i++) {
+
+        $sArray[$i] = $string{$i};
+
+    }
+
+    return $sArray;
+
+}
+
+function slug_noDiacritics($string)
+{
+
+    $cyrylicFrom = array('?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?');
+    $cyrylicTo = array('A', 'B', 'W', 'G', 'D', 'Ie', 'Io', 'Z', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'Ch', 'C', 'Tch', 'Sh', 'Shtch', '', 'Y', '', 'E', 'Iu', 'Ia', 'a', 'b', 'w', 'g', 'd', 'ie', 'io', 'z', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'ch', 'c', 'tch', 'sh', 'shtch', '', 'y', '', 'e', 'iu', 'ia');
+
+    $from = array("Á", "À", "Â", "Ä", "A", "A", "Ã", "Å", "A", "Æ", "C", "C", "C", "C", "Ç", "D", "Ð", "Ð", "É", "È", "E", "Ê", "Ë", "E", "E", "E", "?", "G", "G", "G", "G", "á", "à", "â", "ä", "a", "a", "ã", "å", "a", "æ", "c", "c", "c", "c", "ç", "d", "d", "ð", "é", "è", "e", "ê", "ë", "e", "e", "e", "?", "g", "g", "g", "g", "H", "H", "I", "Í", "Ì", "I", "Î", "Ï", "I", "I", "?", "J", "K", "L", "L", "N", "N", "Ñ", "N", "Ó", "Ò", "Ô", "Ö", "Õ", "O", "Ø", "O", "Œ", "h", "h", "i", "í", "ì", "i", "î", "ï", "i", "i", "?", "j", "k", "l", "l", "n", "n", "ñ", "n", "ó", "ò", "ô", "ö", "õ", "o", "ø", "o", "œ", "R", "R", "S", "S", "Š", "S", "T", "T", "Þ", "Ú", "Ù", "Û", "Ü", "U", "U", "U", "U", "U", "U", "W", "Ý", "Y", "Ÿ", "Z", "Z", "Ž", "r", "r", "s", "s", "š", "s", "ß", "t", "t", "þ", "ú", "ù", "û", "ü", "u", "u", "u", "u", "u", "u", "w", "ý", "y", "ÿ", "z", "z", "ž");
+    $to = array("A", "A", "A", "A", "A", "A", "A", "A", "A", "AE", "C", "C", "C", "C", "C", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "G", "G", "G", "G", "G", "a", "a", "a", "a", "a", "a", "a", "a", "a", "ae", "c", "c", "c", "c", "c", "d", "d", "d", "e", "e", "e", "e", "e", "e", "e", "e", "g", "g", "g", "g", "g", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "IJ", "J", "K", "L", "L", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "CE", "h", "h", "i", "i", "i", "i", "i", "i", "i", "i", "ij", "j", "k", "l", "l", "n", "n", "n", "n", "o", "o", "o", "o", "o", "o", "o", "o", "o", "R", "R", "S", "S", "S", "S", "T", "T", "T", "U", "U", "U", "U", "U", "U", "U", "U", "U", "U", "W", "Y", "Y", "Y", "Z", "Z", "Z", "r", "r", "s", "s", "s", "s", "B", "t", "t", "b", "u", "u", "u", "u", "u", "u", "u", "u", "u", "u", "w", "y", "y", "y", "z", "z", "z");
+
+    $from = array_merge($from, $cyrylicFrom);
+    $to = array_merge($to, $cyrylicTo);
+
+    $newstring = str_replace($from, $to, $string);
+    return $newstring;
+
+}
+
+function makeSlugs($string, $maxlen = 200)
+{
+
+    $newStringTab = array();
+    $tab = null;
+    $stringTab = str_split(strtolower(slug_noDiacritics($string)));
+    $authorizedArray = array_merge(range('a', 'z'), array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'));
+
+    foreach ($stringTab as $letter) {
+
+        if (in_array($letter, $authorizedArray)) {
+
+            $tab = $letter;
+            $newStringTab[] = $tab;
+
+        } else if ($tab != '-') {
+
+            $tab = '-';
+
+            $newStringTab[] = $tab;
+        }
+
+    }
+
+    if (count($newStringTab)) {
+
+        $newString = implode($newStringTab);
+
+        if (!is_null($maxlen)) {
+            $newString = substr($newString, 0, $maxlen);
+        }
+
+    } else {
+
+        $newString = '';
+
+    }
+
+    return trim($newString, '-');
+
+}
+
+function checkSlug($sSlug)
+{
+
+    return preg_match("/^[a-zA-Z0-9]+[a-zA-Z0-9\_\-]*$/", $sSlug);
+
+}
+
+function minify_html($input)
+{
+    if (trim($input) === "") return $input;
+
+
     // Remove extra white-space(s) between HTML attribute(s)
-    $input = preg_replace_callback('#<([^\/\s<>!]+)(?:\s+([^<>]*?)\s*|\s*)(\/?)>#s', function($matches) {
+    $input = preg_replace_callback('#<([^\/\s<>!]+)(?:\s+([^<>]*?)\s*|\s*)(\/?)>#s', function ($matches) {
         return '<' . $matches[1] . preg_replace('#([^\s=]+)(\=([\'"]?)(.*?)\3)?(\s+|$)#s', ' $1$2', $matches[2]) . $matches[3] . '>';
     }, str_replace("\r", "", $input));
     // Minify inline CSS declaration(s)
-    if(strpos($input, ' style=') !== false) {
-        $input = preg_replace_callback('#<([^<]+?)\s+style=([\'"])(.*?)\2(?=[\/\s>])#s', function($matches) {
+    if (strpos($input, ' style=') !== false) {
+        $input = preg_replace_callback('#<([^<]+?)\s+style=([\'"])(.*?)\2(?=[\/\s>])#s', function ($matches) {
             return '<' . $matches[1] . ' style=' . $matches[2] . minify_css($matches[3]) . $matches[2];
         }, $input);
     }
@@ -205,10 +235,10 @@ function load($file, $views = false){
             '#<(img|input)(>| .*?>)<\/\1>#s', // reset previous fix
             '#(&nbsp;)&nbsp;(?![<\s])#', // clean up ...
             '#(?<=\>)(&nbsp;)(?=\<)#' // --ibid<
-			/*
+            /*
             // Remove HTML comment(s) except IE comment(s)
             '#\s*<!--(?!\[if\s).*?-->\s*|(?<!\>)\n+(?=\<[^!])#s'
-			*/
+            */
         ),
         array(
             '<$1$2</$1>',
@@ -222,9 +252,12 @@ function load($file, $views = false){
             '$1',
             ""
         ),
-    $input);
-} function minify_css($input) {
-    if(trim($input) === "") return $input;
+        $input);
+}
+
+function minify_css($input)
+{
+    if (trim($input) === "") return $input;
     return preg_replace(
         array(
             // Remove comment(s)
@@ -262,5 +295,5 @@ function load($file, $views = false){
             '$1:0',
             '$1$2'
         ),
-    $input);
+        $input);
 }
